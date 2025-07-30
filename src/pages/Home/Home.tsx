573801978f5e10d32fc1116f9062d12656f1e510
elmir55 <elmira.050405@gmail.com>
2025-07-30 23:21:25 +0600

    Add Our Doctors at landing section

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,11 +1,13 @@
 import { AboutUs } from '../../components/aboutUs/AboutUs'
+import { OurDoctors } from '../../components/ourDoctors/OurDoctors'
 import { OurServices } from '../../components/ourServices/OurServices'
 
 function Home() {
   return (
     <div>
       < AboutUs />  
-      <OurServices />
+      <OurServices /> 
+      <OurDoctors />
     </div>
   )
 }
