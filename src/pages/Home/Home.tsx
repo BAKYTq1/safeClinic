8992e4c0eabc9c263f3ae00ee30a54dcdc0cc651
Jumaniyaz <zumaniazkozubaev@gmail.com>
2025-07-30 19:33:06 +0600

    CLINIC-93 Landing Implement UI component our clinic and reviews ready

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,11 +1,13 @@
 import { AboutUs } from '../../components/aboutUs/AboutUs'
 import { OurServices } from '../../components/ourServices/OurServices'
+import OurClinic from "../../components/feedback/OurClinic.tsx"
 
 function Home() {
   return (
     <div>
       < AboutUs />  
       <OurServices />
+      <OurClinic/>
     </div>
   )
 }
