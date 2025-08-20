3a98ab4e6ae82ea55523ae65da5bafb60b2a2ff2
Tologon uulu Saridin <saridintologonuulu@gmail.com>
2025-08-20 12:31:55 +0600

    CLINIC-110 Header menu

--- src/widgets/Header/Header.tsx
+++ src/widgets/Header/Header.tsx
@@ -5,14 +5,20 @@ import svg31 from "../../assets/svg/31.svg"
 import svg32 from "../../assets/svg/32.svg"
 import headerImg from "../../assets/svg/header.svg"
 import ProfileModal from "../../widgets/ProfileModal/ProfileModal"
+import header1 from "../../assets/svg/header1.svg"
+import header2 from "../../assets/svg/header2.svg"
+import header3 from "../../assets/svg/header3.svg"
+import header4 from "../../assets/svg/header4.svg"
+import header5 from "../../assets/svg/header5.svg"
 
 
-  
-  // import { useNavigate } from 'react-router-dom'
-  
-  function Header() {
-    // const navigate = useNavigate()
-    const [isModalOpen, setIsModalOpen] = useState(false);
+
+// import { useNavigate } from 'react-router-dom'
+
+function Header() {
+  // const navigate = useNavigate()
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [menuOpen, setMenuOpen] = useState(false);
   return (
     <div className='header'>
       <div className='header-container'>
@@ -32,14 +38,42 @@ import ProfileModal from "../../widgets/ProfileModal/ProfileModal"
           <button>Запись онлайн</button>
         </div>
 
-        <div className='header-img'>
-          <img src={svg32} alt="" />
-        </div>
-
         <div className="header-i" onClick={() => setIsModalOpen(true)}>
           <img src={headerImg} alt="Профиль" />
         </div>
         {isModalOpen && <ProfileModal onClose={() => setIsModalOpen(false)} />}
+        <div className="header-img" onClick={() => setMenuOpen(!menuOpen)}>
+          <img src={svg32} alt="burger" />
+        </div>
+
+        {menuOpen && (
+          <div className={`header-menu ${menuOpen ? "active" : ""}`}>
+            <div className="header-menu-i">
+              <div className="menu-item">
+                <img className="header1" src={header1} alt="header 1" />
+                <p>О нас</p>
+              </div>
+              <div className="menu-item">
+                <img className="header1"  src={header2} alt="header 2" />
+                <p>Услуги</p>
+              </div>
+              <div className="menu-item">
+                <img className="header1"  src={header3} alt="header 3" />
+                <p>Врачи</p>
+              </div>
+              <div className="menu-item">
+                <img className="header1"  src={header4} alt="header 4" />
+                <p>Наша клиника</p>
+              </div>
+              <div className="menu-item">
+                <img className="header1"  src={header5} alt="header 5" />
+                <p>Контакт</p>
+              </div>
+            </div>
+          </div>
+
+        )}
+
 
       </div>
     </div>
