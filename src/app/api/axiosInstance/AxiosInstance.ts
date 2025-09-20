8bd6ce5e570bd34cb3624e60b4e926441cf473ef
Jumaniyaz <zumaniazkozubaev@gmail.com>
2025-09-20 11:28:58 +0600

    CLINIC-109

--- src/app/api/axiosInstance/AxiosInstance.ts
+++ src/app/api/axiosInstance/AxiosInstance.ts
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { useApiStore } from "../apiStore/apiStore";
+import { useApiStore } from "../apiStore/ApiStore"
 
 export const API_URL = "https://safeclinic-production.up.railway.app";
 
